863dc3eb4a654e2870ff3673fe9a76a5dd1b3182
srikar <mandli.srikar95@gmail.com>
2021-08-17 15:28:16 -0400

    first commit

---
+++ learning_templates/learning_templates/my_extra.py
@@ -0,0 +1,10 @@
+from django import template
+
+register = template.Library()
+@register.filter(name='cut')
+def cut(value,arg):
+    """
+    This cuts all the values of arg from the string!
+    """
+
+    return value.replace(arg,'')
